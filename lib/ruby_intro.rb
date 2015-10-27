1e4ab0fdcd8f9cbdfb58c40b5159cd61019db3d6
williamcs <solobird00@hotmail.com>
2015-10-27 09:07:46 +0000

    finished the code

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,70 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  s = 0
+  arr.each { |e| s += e }
+  return s
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.size == 0
+    return 0
+  elsif arr.size == 1
+    return arr[0]
+  else
+    sorted_array = arr.sort
+    return sorted_array[sorted_array.size - 1] + sorted_array[sorted_array.size - 2]
+  end
+  
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.size < 2
+  
+  arr.length.times do |i|
+      arr.length.times do |j|
+          if i != j && arr[i] + arr[j] == n
+              return true
+          end
+      end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if !(s =~ /^[^AEIOU\W]/i)
+    return false
+  end
+  return true
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  #!!(s =~ /(^0$|^(0|1)+0{2}$)/)
+  !!((s =~ /^0+$|^[01]*0{2}$/))
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def initialize(isbn, price)
+    if isbn.empty? || price <= 0
+      raise(ArgumentError)
+    end
+    
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    return '$%.2f' % @price
+  end
 end
